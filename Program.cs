3a768208dc394b96aee5b695d5f23e7fe1cd13aa
Gabriel Salvador <gabrielgarciasalvador@outlook.com>
2019-11-14 21:18:18 -0300

    inicializado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace furb_cg_trabalho_final_meinkraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
