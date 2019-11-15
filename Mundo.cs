9f80fb236960e81938053beaf17d4f2b22a770eb
gabrielgsalvatore <gabrielgarciasalvador@outlook.com>
2019-11-15 18:28:32 -0300

    Iniciando

---
+++ Mundo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using System.Collections.Generic;
+using OpenTK.Input;
+
+
+
+namespace furb_cg_trabalho_final_meinkraft
+{
+    class Mundo : GameWindow
+    {
+        private static Mundo singletonMundo = null;
+
+        private Mundo(int width, int height) : base(width, height) { }
+
+        public static Mundo GetInstance(int width, int height){
+            if(singletonMundo == null)
+                singletonMundo = new Mundo(width, height);
+            return singletonMundo;
+        }
+
+
+        protected override void OnLoad(EventArgs e)
+        {
+        base.OnLoad(e);
+        GL.ClearColor(Color.Gray);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+        base.OnUpdateFrame(e);
+        GL.MatrixMode(MatrixMode.Projection);
+        GL.LoadIdentity();
+        GL.Ortho(-300, 300, -300, 300, -10, 10);
+        }
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+        base.OnRenderFrame(e);
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+        GL.MatrixMode(MatrixMode.Modelview);
+        GL.LoadIdentity();   
+
+
+        this.SwapBuffers();
+        }
+
+      }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
-
-namespace furb_cg_trabalho_final_meinkraft
-{
-    class Program
+  using furb_cg_trabalho_final_meinkraft;
+  
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Mundo window = Mundo.GetInstance(600, 600);
+      window.Title = "Meinkraft";
+      window.Run(1.0 / 60.0);
     }
-}
+  }
